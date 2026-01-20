93dee4cbc50e6c04b21671f98bd941d1f69142c7
Mettymagic <metty.metamagic@gmail.com>
2026-01-20 16:23:03 -0700

    icon

--- src/config.ts
+++ src/config.ts
@@ -31,11 +31,14 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+			src: "/favicon/favicon-32x32.png", // Path of the favicon, relative to the /public directory
+			sizes: "32x32", // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		},
+		{
+			src: "/favicon/android-chrome-512x512.png", // Path of the favicon, relative to the /public directory
+			sizes: "512x512", // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		},
 	],
 };
 
